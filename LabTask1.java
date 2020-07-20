f8ddd6f1dc7b5c646818abcc937e1c1fb90ddfc6
Naman Seth <naman.seth_cs19@gla.ac.in>
2020-07-20 11:59:13 +0530

    Lab Tasks....

---
+++ LabTask1.java
@@ -0,0 +1,14 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Naman Seth (namanseth01)
+ *   Date: 20-Jul-20
+ *   Time: 11:33 AM
+ *   File: LabTask1.java
+ */
+
+package labProblems;
+
+public class LabTask1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!!");
+    }
+}
---
+++ LabTask2.java
@@ -0,0 +1,29 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Naman Seth (namanseth01)
+ *   Date: 20-Jul-20
+ *   Time: 11:34 AM
+ *   File: LabTask2.java
+ */
+
+package labProblems;
+
+public class LabTask2 {
+    public static void main(String[] args) {
+        byte byteValue = 5;
+        short shortValue = 1000;
+        char characterValue = '@';
+        int integerValue = 5000;
+        long longValue = 6235745l;
+        float floatValue = 23.67f;
+        double doubleValue = 56.3256;
+        boolean booleanValue = true;
+        System.out.println("Byte= " + byteValue);
+        System.out.println("Short= " + shortValue);
+        System.out.println("Character= " + characterValue);
+        System.out.println("Integer= " + integerValue);
+        System.out.println("Long= " + longValue);
+        System.out.println("Float= " + floatValue);
+        System.out.println("Double= " + doubleValue);
+        System.out.println("Boolean= " + booleanValue);
+    }
+}
